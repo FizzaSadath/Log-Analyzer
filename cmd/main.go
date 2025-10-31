4db96857a74692c2627c595d36c9658c94f0949a
Fizza Sadath <fizzasadath@gmail.com>
2025-10-31 10:32:35 +0530

    Implements test case for filtering based on indexing.

--- cmd/main.go
+++ cmd/main.go
@@ -1,8 +1,14 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"log/slog"
+	"log_analyzer/filter"
 	"log_analyzer/segmenter"
+	"time"
+
+	"strings"
 )
 
 func main() {
@@ -11,6 +17,51 @@ func main() {
 	// 	fmt.Println(entry)
 	// }
 	// fmt.Println(len(entries))
-	logStore, _ := segmenter.ParseLogSegments("../logs")
-	fmt.Println(logStore.Segments[0])
+	level := flag.String("level", "", "Filter by log level")
+	component := flag.String("component", "", "Filter by component")
+	host := flag.String("host", "", "Filter by host")
+	reqID := flag.String("reqID", "", "Filter by requestID")
+	startTimeString := flag.String("after", "", "Filter by start time")
+	endTimeString := flag.String("before", "", "Filter by end time")
+	flag.Parse()
+
+	logStore, err := segmenter.ParseLogSegments("../logs")
+	// fmt.Println(logStore.Segments[0])
+	if err != nil {
+		slog.Error("Failed to parse logs\n")
+	}
+	split := func(s string) []string {
+		if s == "" {
+			return nil
+		}
+		parts := strings.Split(s, ",")
+		for i := range parts {
+			parts[i] = strings.TrimSpace(parts[i])
+		}
+		return parts
+	}
+	levels := split(*level)
+	components := split(*component)
+	hosts := split(*host)
+	reqIDs := split(*reqID)
+	var startTime, endTime time.Time
+	if *startTimeString != "" {
+		startTime, err = time.Parse("2006-01-02 15:04:05", *startTimeString)
+		if err != nil {
+			slog.Error("Error parsing start time", "error", err)
+		}
+	}
+
+	if *endTimeString != "" {
+		endTime, err = time.Parse("2006-01-02 15:04:05", *endTimeString)
+		if err != nil {
+			slog.Error("Error parsing end time", "error", err)
+		}
+	}
+
+	filteredLogs := filter.FilterLogs(logStore, levels, components, hosts, reqIDs, startTime, endTime)
+	fmt.Printf("Found %d matching entries\n", len(filteredLogs))
+	for _, entry := range filteredLogs {
+		fmt.Println(entry.Raw)
+	}
 }
