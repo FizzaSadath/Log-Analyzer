0e2e735c1ae6a1ed6beac4ef69770343755961b3
Fizza Sadath <fizzasadath@gmail.com>
2025-11-02 18:14:38 +0530

    Add goroutines to parse each log file to segments in parallel.

--- segmenter/logSegmenter.go
+++ segmenter/logSegmenter.go
@@ -8,50 +8,77 @@ import (
 	"log_analyzer/parser"
 	"os"
 	"path/filepath"
+	"sync"
+	"time"
 )
 
 func ParseLogSegments(path string) (models.LogStore, error) {
-	LogStore := models.LogStore{
+	start := time.Now()
+	logStore := models.LogStore{
 		Segments: []models.Segment{},
 	}
+
 	files, err := os.ReadDir(path)
 	if err != nil {
-		return LogStore, fmt.Errorf("failed to read directory : %v", err)
+		return logStore, fmt.Errorf("failed to read directory: %v", err)
 	}
+
+	var wg sync.WaitGroup
+	var mu sync.Mutex // protects logStore.Segments
+
 	for _, file := range files {
 		if file.IsDir() {
 			continue
 		}
-		filepath := filepath.Join(path, file.Name())
-		f, err := os.Open(filepath)
-		if err != nil {
-			fmt.Printf("Skipping file %s due to error: %v", filepath, err)
-		}
-		var LogEntries []models.LogEntry
-		scanner := bufio.NewScanner(f)
-		scanner.Buffer(make([]byte, 0, 1024*1024), 10*1024*1024) // allow 10MB lines
 
-		for scanner.Scan() {
-			line := scanner.Text()
-			entry, err := parser.ParseLogEntry(line)
-			if err == nil {
-				LogEntries = append(LogEntries, *entry)
+		wg.Add(1)
+		go func(file os.DirEntry) {
+			defer wg.Done()
+
+			filepath := filepath.Join(path, file.Name())
+			f, err := os.Open(filepath)
+			if err != nil {
+				fmt.Printf("Skipping file %s due to error: %v\n", filepath, err)
+				return
 			}
-		}
-		f.Close()
-		if len(LogEntries) == 0 {
-			continue
-		}
-		index := indexer.BuildSegmentIndex(LogEntries)
-		segment := models.Segment{
-			FileName:   file.Name(),
-			LogEntries: LogEntries,
-			StartTime:  LogEntries[0].Time,
-			EndTime:    LogEntries[len(LogEntries)-1].Time,
-			Index:      index,
-		}
-		LogStore.Segments = append(LogStore.Segments, segment)
-	}
-	return LogStore, nil
+			defer f.Close()
 
+			var logEntries []models.LogEntry
+			scanner := bufio.NewScanner(f)
+			scanner.Buffer(make([]byte, 0, 1024*1024), 10*1024*1024) // allow 10MB lines
+
+			for scanner.Scan() {
+				line := scanner.Text()
+				entry, err := parser.ParseLogEntry(line)
+				if err == nil {
+					logEntries = append(logEntries, *entry)
+				}
+			}
+
+			if len(logEntries) == 0 {
+				return
+			}
+
+			index := indexer.BuildSegmentIndex(logEntries)
+			segment := models.Segment{
+				FileName:   file.Name(),
+				LogEntries: logEntries,
+				StartTime:  logEntries[0].Time,
+				EndTime:    logEntries[len(logEntries)-1].Time,
+				Index:      index,
+			}
+
+			// lock before writing to shared slice
+			mu.Lock()
+			logStore.Segments = append(logStore.Segments, segment)
+			mu.Unlock()
+
+		}(file)
+	}
+
+	wg.Wait()
+
+	elapsed := time.Since(start)
+	fmt.Println("Segment parsing took:", elapsed)
+	return logStore, nil
 }
